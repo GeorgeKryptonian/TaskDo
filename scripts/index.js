0c63664b14eb8f2f309e3c12a71fda97603cffe3
GeorgeKryptonian <54601856+GeorgeKryptonian@users.noreply.github.com>
2022-04-26 16:31:54 +0200

    Upload

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ let letsStartIntroBlock = document.querySelector('.letsStartIntroBlock');
 let letsStartIntroButton = document.querySelector('.letsStartIntroButton');
 let letsStartInputBlock = document.querySelector('.letsStartInputBlock');
 let letsStartInputName = document.querySelector('.letsStartInputName');
-let hr = document.querySelector('hr');
+let hr = document.querySelector('.hr');
 
 anime({
     targets: '.letsStartIntroBlock',
@@ -23,7 +23,7 @@ letsStartIntroButton.addEventListener('click', (target) => {
         letsStartInputBlock.classList.remove('hidden');
         anime({
             targets: '.letsStartInputBlock',
-            translateY: (window.innerHeight - letsStartInputBlock.clientHeight) / 2,
+            translateY: (window.innerHeight - (letsStartInputBlock.clientHeight + 15 + 35)) / 2,
             opacity: '1',
             easing: 'easeInOutQuad',
         });
@@ -34,17 +34,27 @@ letsStartInputName.addEventListener('input', (event) => {
     if (event.target.value.length >= event.target.minLength && !event.target.value.slice(0, 2).includes(' ')) {
         anime({
             targets: hr,
-            borderColor: '#78B159',
-            width: '100%',
-            easing: 'easeInOutQuad',
+            keyframes: [
+                {opacity: 1, width: '50%', marginTop: '15px'},
+                {fontSize: '16px', height: '35px'}
+            ],
+            duration: 1500,
+            easing: 'easeOutElastic',
         })
+        letsStartInputName.classList.remove('outline-gray-300', 'text-gray-300');
+        letsStartInputName.classList.add('outline-green-500', 'text-green-500');
     } else {
         anime({
             targets: hr,
-            borderColor: '#FFFFFF',
+            height: '3px',
+            fontSize: 0,
+            marginTop: '3px',
             width: 0,
-            easing: 'easeInOutQuad',
+            opacity: 0,
+            duration: 1500,
+            easing: 'easeOutElastic',
         })
+        letsStartInputName.classList.remove('outline-green-500', 'text-green-500');
+        letsStartInputName.classList.add('outline-gray-300', 'text-gray-300');
     }
 })
-
